e2e2f603684dd3647573672628804d4740f886a0
Matheus Freire <matheus.sfreire@gmail.com>
2022-12-19 21:59:25 -0300

    Add delegate to UiTextField

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -9,17 +9,56 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
                         UINavigationControllerDelegate {
-
-    @IBOutlet weak var imageView: UIImageView!
     
+    // MARK: Properties
+    
+    let textFieldDelegate = MemeMeTextFieldDelegate()
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  3.0
+    ]
+    
+    // MARK: Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    // MARK: Life Cy;e
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        initializeTextField(topTextField)
+        initializeTextField(bottomTextField)
     }
+    
+    private func initializeTextField(_ textField: UITextField) {
+        textField.text = "NEW TEXT"
+        textField.textAlignment = .center
+        textField.delegate = textFieldDelegate
+        textField.defaultTextAttributes = memeTextAttributes
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    // MARK: Actions
 
-    @IBAction func pickAnImage(_ sender:Any) {
+    @IBAction func pickAnImageFromAlbum(_ sender:Any) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender:Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
         present(pickerController, animated: true, completion: nil)
     }
     
---
+++ MemeMeTextFieldDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  MemeMeTextFieldDelegate.swift
+//  Meme Me
+//
+//  Created by Matheus da Silva Freire on 15/12/22.
+//
+
+import Foundation
+import UIKit
+
+class MemeMeTextFieldDelegate : NSObject, UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+    
+}
