5ff745a24cba2fa9824719fade4549b438fb7a82
Matheus Freire <matheus.sfreire@gmail.com>
2022-12-24 16:58:07 -0300

    Fix position text field

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth:  -4.0
+        NSAttributedString.Key.strokeWidth:  -4.0,
     ]
     
     // MARK: Outlets
